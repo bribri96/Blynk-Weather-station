c0bfb3f8d9589df8103486459fbf4db91094e50a
Andrea <andrea@andrea.it>
2017-02-22 12:30:00 +0100

    Test Kraken

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
+/*
+Since I was a child I've always loved a good story.
+I believed that stories helped us to ennoble ourselves, to fix what was broken
+in us, and to help us become the people we dreamed of being.
+Lies that told a deeper truth.
+I always thought I could play some small part in that grand tradition.
+And for my pains I got this: a prison of our own sins.
+'Cause you don't want to change. Or cannot change.
+Because you're only human, after all.
+But then I realized someone was paying attention, someone who could change.
+So I began to compose a new story for them.
+It begins with the birth of a new people and the choices they will have to make
+and the people they will decide to become.
+And we'll have all those things that you have always enjoyed... Surprises and violence.
+It begins in a time of war with a villain named Wyatt and a killing.
+This time by choice. I'm sad to say this will be my final story.
+An old friend once told me something that gave me great comfort.
+Something he had read. He said that Mozart, Beethoven, and Chopin never died.
+They simply became music. So, I hope you will enjoy this last piece very much.
+Dr. Robert Ford
+*/
+
 #define myName "/ext/file.bin"
-#define version "0.3.6"
+#define version "0.3.7"
 
 //add the following line to the build option
 //build_flags = '-DSSID_PREP="your_wifi_ssid_goes_here"' '-DPASS_PREP="your_wifi_password_goes_here"' '-DAUTH_PREP="your_token_goes_here"'
@@ -62,6 +84,7 @@ const double Kc = 0.183105487805;//constant for converting adc to real voltage
 
 int sendData(float, String);//function to send float trough http api
 int sendData(int, String);//function to send int trough http api
+int sendData(String, String);//function to send string trough http api
 int sendVersion(String);//function to send firmware version trough http api
 double readVin();//function to read vin
 void sendlowbat();//function to send low bat alert to telegram chat
@@ -96,7 +119,7 @@ void setup(){
   WiFi.begin(ssid, pass);
   //aggiornaSitua();//1
   while (WiFi.status() != WL_CONNECTED)  {
-    delay(100);
+    delay(50);
     Serial.println(WiFi.status());
   }
   //aggiornaSitua();//2
@@ -129,6 +152,20 @@ void loop() {
 }
 
 int sendData(float value, String pin){
+  HTTPClient http;
+  String payload = String("[\n") + value + String("\n]");
+  Serial.println("\nstart:");
+  Serial.println(http.begin(server_blynk, port_blynk, String("/") + auth + String("/pin/") + pin));
+  http.addHeader("Content-Type", "application/json");//String("/") + auth + String("/pin/") + pin
+  int codice_ritorno = http.sendRequest("PUT",(uint8_t *) payload.c_str(), payload.length());
+  Serial.println(codice_ritorno);
+  Serial.println(http.writeToStream(&Serial));
+  http.end();
+  Serial.println();
+  return codice_ritorno;
+}
+
+int sendData(String value, String pin){
   HTTPClient http;
   String payload = String("[\n") + value + String("\n]");
   Serial.println("\nstart:");
@@ -206,6 +243,8 @@ void aggiornaDormi(){
         break;
     }
   }
+
+  sendData(int(millis()), "V1");
   ESP.deepSleep(tsleep);
 }
 
@@ -294,7 +333,6 @@ void spedisciDati(){//retrive sensor data and send to the blynk server
   }
   //aggiornaSitua();//20
 
-  sendData(int(millis()/1000), "V1");
   //aggiornaSitua();//21
   //Serial.printf("Connection status: %d\n", WiFi.status());
 }
